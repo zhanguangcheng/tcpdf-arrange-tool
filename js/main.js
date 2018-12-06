f788d66fcd021a54852e29f3edaa816eeba96394
zhanguangcheng <14712905@qq.com>
2018-12-06 10:05:04 +0800

    update

--- js/main.js
+++ js/main.js
@@ -540,6 +540,7 @@
 
     var getPaperSize = function () {
         var maps = {
+            'A3-L': { width: 420, height: 297 },
             A3: { width: 297, height: 420 },
             A4: { width: 210, height: 297 },
             A5: { width: 148, height: 210 },
